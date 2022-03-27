4a027682006fd81a0610b3be54d9a0f5c3713ed3
Rajesh Prabhu <rajesh.prabhu@publicissapient.com>
2022-03-27 15:13:17 +0530

    Add map to register

--- src/components/Auth/Register.jsx
+++ src/components/Auth/Register.jsx
@@ -15,6 +15,7 @@ import { useFormik } from "formik";
 import * as Yup from "yup";
 import { registerUser } from "../../redux/actions/users";
 import { useSelector, useDispatch } from "react-redux";
+import { usePlacesWidget } from "react-google-autocomplete";
 
 const phoneRegExp =
   /^((\\+[1-9]{1,4}[ \\-]*)|(\\([0-9]{2,3}\\)[ \\-]*)|([0-9]{2,4})[ \\-]*)*?[0-9]{3,4}?[ \\-]*[0-9]{3,4}?$/;
@@ -27,6 +28,8 @@ const roles = [
 export default function Register() {
   const dispatch = useDispatch();
   const loading = useSelector(state => state.users.loading);
+
+  const [addressObject, setAddressObject] = React.useState({});
 
   const SignupSchema = Yup.object().shape({
     name: Yup.string()
@@ -66,163 +69,244 @@ export default function Register() {
             role:  values.role,
             contactNo: values.contact,
             email: values.email,
-          //   implement once address component ready
-            address: {
-              district: "test",
-              state: "test",
-              pincode: "012345",
-              lat: "test",
-              lng: "test",
-            },
+            address: addressObject,
           };
           dispatch(registerUser(user));
     },
   });
 
-  return (
-    <Container component='main' maxWidth='xs'>
-      <CssBaseline />
-      <Box
-        sx={{
-          marginTop: 8,
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-        }}>
-        <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
-          <LockOutlinedIcon />
-        </Avatar>
-        <Typography component='h1' variant='h5'>
-          Sign up
-        </Typography>
+    const getAddressComponent = (place, componentName, property) => {
+        var comps = place.address_components.filter(function (component) {
+            return component.types.indexOf(componentName) !== -1;
+        });
 
-        <Box
-          component='form'
-          noValidate
-          onSubmit={formik.handleSubmit}
-          sx={{ mt: 3 }}>
-          <Grid container spacing={2}>
-            <Grid item xs={12}>
-              <TextField
-                autoComplete='given-name'
-                name='name'
-                required
-                fullWidth
-                id='name'
-                label='Name'
-                value={formik.values.name}
-                onChange={formik.handleChange}
-                error={formik.touched.name && Boolean(formik.errors.name)}
-                helperText={formik.touched.name && formik.errors.name}
-                autoFocus
-                placeholder='Enter Full Name'
-              />
-            </Grid>
-            <Grid item xs={12}>
-              <TextField
-                name='userName'
-                required
-                fullWidth
-                id='userName'
-                label='User Name'
-                value={formik.values.userName}
-                onChange={formik.handleChange}
-                error={
-                  formik.touched.userName && Boolean(formik.errors.userName)
-                }
-                helperText={formik.touched.userName && formik.errors.userName}
-                placeholder='Enter User Name'
-              />
-            </Grid>
-            <Grid item xs={12}>
-              <TextField
-                name='role'
-                required
-                fullWidth
-                select
-                id='role'
-                label='Role'
-                value={formik.values.role}
-                onChange={formik.handleChange}
-                error={formik.touched.role && Boolean(formik.errors.role)}
-                helperText={formik.touched.role && formik.errors.role}
-                placeholder='Select Role'>
-                {roles.map((role) => (
-                  <MenuItem key={role.name} value={role.name}>
-                    {role.label}
-                  </MenuItem>
-                ))}
-              </TextField>
-            </Grid>
-            <Grid item xs={12}>
-              <TextField
-                required
-                fullWidth
-                id='contact'
-                label='Contact Number'
-                name='contact'
-                value={formik.values.contact}
-                onChange={formik.handleChange}
-                error={formik.touched.contact && Boolean(formik.errors.contact)}
-                helperText={formik.touched.contact && formik.errors.contact}
-                placeholder='Enter Contact Number'
-              />
-            </Grid>
-            <Grid item xs={12}>
-              <TextField
-                required
-                fullWidth
-                id='email'
-                name='email'
-                label='Email'
-                value={formik.values.email}
-                onChange={formik.handleChange}
-                error={formik.touched.email && Boolean(formik.errors.email)}
-                helperText={formik.touched.email && formik.errors.email}
-              />
-            </Grid>
-            <Grid item xs={12}>
-              <TextField
-                required
-                fullWidth
-                name='password'
-                label='Password'
-                type='password'
-                id='password'
-                value={formik.values.password}
-                onChange={formik.handleChange}
-                error={
-                  formik.touched.password && Boolean(formik.errors.password)
-                }
-                helperText={formik.touched.password && formik.errors.password}
-                autoComplete='new-password'
-              />
-            </Grid>
-          </Grid>
-          <Button
-            type='submit'
-            fullWidth
-            variant='contained'
-            sx={{
-              mt: 3,
-              mb: 2,
-              bgcolor: "#dd4343",
-              ":hover": {
-                bgcolor: "#ac3434",
-              },
-            }}>
-            Sign Up
-          </Button>
-          <Button
-            type='button'
-            href='/login'
-            fullWidth
-            variant='outlined'
-            sx={{ mt: 3, mb: 2, textTransform: "none" }}>
-            Already Registered, Log In
-          </Button>
-        </Box>
-      </Box>
-    </Container>
+        if (comps && comps.length && comps[0] && comps[0][property]) {
+            return comps[0][property];
+        } else {
+            return null;
+        }
+    };
+
+    const updateAddressObject = (place) => {
+
+        const address = {
+            district: getAddressComponent(place, 'administrative_area_level_2', 'long_name'),
+            state: getAddressComponent(place, 'administrative_area_level_1', 'long_name'),
+            pincode: "123456",
+            lat: place.geometry.location.lat(),
+            lng: place.geometry.location.lng(),
+        };
+
+        setAddressObject(address);
+    };
+
+    const { ref } = usePlacesWidget({
+        apiKey: process.env.REACT_APP_GOOGLE_API_KEY,
+        onPlaceSelected: (place) => {
+            updateAddressObject(place);
+            formik.setFieldValue("address", place.formatted_address);
+        },
+    });
+
+  return (
+      <Container component="main" maxWidth="xs">
+          <CssBaseline />
+          <Box
+              sx={{
+                  marginTop: 8,
+                  display: "flex",
+                  flexDirection: "column",
+                  alignItems: "center",
+              }}
+          >
+              <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+                  <LockOutlinedIcon />
+              </Avatar>
+              <Typography component="h1" variant="h5">
+                  Sign up
+              </Typography>
+
+              <Box
+                  component="form"
+                  noValidate
+                  onSubmit={formik.handleSubmit}
+                  sx={{ mt: 3 }}
+              >
+                  <Grid container spacing={2}>
+                      <Grid item xs={12}>
+                          <TextField
+                              autoComplete="given-name"
+                              name="name"
+                              required
+                              fullWidth
+                              id="name"
+                              label="Name"
+                              value={formik.values.name}
+                              onChange={formik.handleChange}
+                              error={
+                                  formik.touched.name &&
+                                  Boolean(formik.errors.name)
+                              }
+                              helperText={
+                                  formik.touched.name && formik.errors.name
+                              }
+                              autoFocus
+                              placeholder="Enter Full Name"
+                          />
+                      </Grid>
+                      <Grid item xs={12}>
+                          <TextField
+                              name="userName"
+                              required
+                              fullWidth
+                              id="userName"
+                              label="User Name"
+                              value={formik.values.userName}
+                              onChange={formik.handleChange}
+                              error={
+                                  formik.touched.userName &&
+                                  Boolean(formik.errors.userName)
+                              }
+                              helperText={
+                                  formik.touched.userName &&
+                                  formik.errors.userName
+                              }
+                              placeholder="Enter User Name"
+                          />
+                      </Grid>
+                      <Grid item xs={12}>
+                          <TextField
+                              name="role"
+                              required
+                              fullWidth
+                              select
+                              id="role"
+                              label="Role"
+                              value={formik.values.role}
+                              onChange={formik.handleChange}
+                              error={
+                                  formik.touched.role &&
+                                  Boolean(formik.errors.role)
+                              }
+                              helperText={
+                                  formik.touched.role && formik.errors.role
+                              }
+                              placeholder="Select Role"
+                          >
+                              {roles.map((role) => (
+                                  <MenuItem key={role.name} value={role.name}>
+                                      {role.label}
+                                  </MenuItem>
+                              ))}
+                          </TextField>
+                      </Grid>
+                      <Grid item xs={12}>
+                          <TextField
+                              required
+                              fullWidth
+                              id="address"
+                              label="Address"
+                              name="address"
+                              value={formik.values.address}
+                              onChange={formik.handleChange}
+                              inputRef={ref}
+                              error={
+                                  formik.touched.address &&
+                                  Boolean(formik.errors.address)
+                              }
+                              helperText={
+                                  formik.touched.address &&
+                                  formik.errors.address
+                              }
+                              placeholder="Enter Address"
+                          />
+                      </Grid>
+                      <Grid item xs={12}>
+                          <TextField
+                              required
+                              fullWidth
+                              id="contact"
+                              label="Contact Number"
+                              name="contact"
+                              value={formik.values.contact}
+                              onChange={formik.handleChange}
+                              error={
+                                  formik.touched.contact &&
+                                  Boolean(formik.errors.contact)
+                              }
+                              helperText={
+                                  formik.touched.contact &&
+                                  formik.errors.contact
+                              }
+                              placeholder="Enter Contact Number"
+                          />
+                      </Grid>
+                      <Grid item xs={12}>
+                          <TextField
+                              required
+                              fullWidth
+                              id="email"
+                              name="email"
+                              label="Email"
+                              value={formik.values.email}
+                              onChange={formik.handleChange}
+                              error={
+                                  formik.touched.email &&
+                                  Boolean(formik.errors.email)
+                              }
+                              helperText={
+                                  formik.touched.email && formik.errors.email
+                              }
+                          />
+                      </Grid>
+                      <Grid item xs={12}>
+                          <TextField
+                              required
+                              fullWidth
+                              name="password"
+                              label="Password"
+                              type="password"
+                              id="password"
+                              value={formik.values.password}
+                              onChange={formik.handleChange}
+                              error={
+                                  formik.touched.password &&
+                                  Boolean(formik.errors.password)
+                              }
+                              helperText={
+                                  formik.touched.password &&
+                                  formik.errors.password
+                              }
+                              autoComplete="new-password"
+                          />
+                      </Grid>
+                  </Grid>
+                  <Button
+                      type="submit"
+                      fullWidth
+                      variant="contained"
+                      sx={{
+                          mt: 3,
+                          mb: 2,
+                          bgcolor: "#dd4343",
+                          ":hover": {
+                              bgcolor: "#ac3434",
+                          },
+                      }}
+                  >
+                      Sign Up
+                  </Button>
+                  <Button
+                      type="button"
+                      href="/login"
+                      fullWidth
+                      variant="outlined"
+                      sx={{ mt: 3, mb: 2, textTransform: "none" }}
+                  >
+                      Already Registered, Log In
+                  </Button>
+              </Box>
+          </Box>
+      </Container>
   );
 }
--- src/components/UI/Button/Button.jsx
+++ src/components/UI/Button/Button.jsx
@@ -6,10 +6,14 @@ export default (props) => {
     const { children, variant, type, path, className, onClick } = props;
 
     //variant can be => text/outlined/contained
+    
+    const routeLink = () => {
+        navigate(path, { replace: false });
+    }
 
     if (type === "link") {
         return (
-            <Button href={path} variant={variant} className={`${classes[variant]} ${className}`}>{children}</Button>
+            <Button href={path} variant={variant} className={`${classes[variant]} ${className}`} onClick={routeLink}>{children}</Button>
         );
     } else {
         return (
