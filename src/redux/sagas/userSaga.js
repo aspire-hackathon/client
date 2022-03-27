19e89c2a32d4f4a724a8f24175b2e420d1ff218e
System Administrator <root@WKMZTB7C80DC.global.publicisgroupe.net>
2022-03-27 14:30:57 +0530

    userby id and uname - reg update onsubmit

--- src/redux/sagas/userSaga.js
+++ src/redux/sagas/userSaga.js
@@ -1,6 +1,7 @@
 import { call, put, takeEvery } from 'redux-saga/effects';
 import * as types from '../types/users';
 import axios from 'axios';
+import users from '../reducers/users';
 
 function registerApi(user) {
     return axios.post(process.env.REACT_APP_API_URL+'/register',user)
@@ -17,9 +18,48 @@ function* registerUser(action) {
     }
 }
 
-function* userSaga() {
-    // yield takeEvery(types.GET_USERS_REQUESTED, fetchUsers);
+export function* registerUserSaga() {
+    console.log("inside saga")
     yield takeEvery(types.REGISTER_USER, registerUser);
 }
 
-export default userSaga;
+function userByIdApi(id){
+    console.log('saga',id)
+    return axios.get(process.env.REACT_APP_API_URL+`/user/byid/${id}`)
+    .then(res => res.data)
+    .catch((error) => {throw error})
+}
+
+function* getUserbyId(action) {
+    try{
+        const user = yield call(userByIdApi,action.payload);
+        yield put({ type: types.GET_USER_BYID_SUCCESS, user: user});
+    } catch (e) {
+        yield put({ type: types.GET_USER_BYID_FAILURE, message: e.message })
+    }
+}
+
+export function* getUserByIdSaga() {
+    yield takeEvery(types.GET_USER_BYID, getUserbyId);
+}
+
+function userByUnameApi(username){
+    console.log('saga',username)
+    return axios.get(process.env.REACT_APP_API_URL+`/user/byid/${id}`)
+    .then(res => res.data)
+    .catch((error) => {throw error})
+}
+
+function* getUserbyUname(action) {
+    try{
+        const user = yield call(userByUnameApi,action.payload);
+        yield put({ type: types.GET_USER_BYID_SUCCESS, user: user});
+    } catch (e) {
+        yield put({ type: types.GET_USER_BYID_FAILURE, message: e.message })
+    }
+}
+
+export function* getUserByUnameSaga() {
+    yield takeEvery(types.GET_USER_BYUN, getUserbyUname);
+}
+
